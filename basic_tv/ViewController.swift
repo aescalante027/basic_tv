0186e8ada73a91dce34372873533fd9b870e92ee
Alex Escalante <alexnescalante@shinanju.local>
2017-10-02 17:06:07 -0700

    Initial tvOS project commit

--- basic_tv/ViewController.swift
+++ basic_tv/ViewController.swift
@@ -10,16 +10,36 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var score = Int()
+    
+    @IBOutlet weak var scoreLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
 
-
+    @IBAction func Add(_ sender: Any) {
+        score = score + 1
+        ShowScore()
+    }
+    
+    @IBAction func Subtract(_ sender: Any) {
+        score = score - 1
+        ShowScore()
+    }
+    @IBAction func Reset(_ sender: Any) {
+        score = 0
+        ShowScore()
+    }
+    
+    func ShowScore() {
+        scoreLabel.text = "Score: \(score)"
+    }
 }
 
